5baa4cb9c3d381d3cbdb36836be5b83ea9c7a9b6
YashSuvagiyaDeveloper <137169748+YashSuvagiyaDeveloper@users.noreply.github.com>
2023-12-13 08:50:32 +0530

    "final commit"

---
+++ src/components/global/Footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div>
+            <footer>
+                <div className="container">
+                    <div className="col-lg-8">
+                        <p className='mb-0'>Copyright © Villa Agency Co., Ltd. All rights reserved.
+                        </p>
+                    </div>
+                </div>
+            </footer>
+        </div>
+    )
+}
+
+export default Footer
